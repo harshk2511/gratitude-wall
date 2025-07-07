c054a156adbe45667a7bcd894892464764aa5957
Harshitha Kambham <harshk2511@gmail.com>
2025-07-07 18:54:25 +0530

    First Commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        createPost: 'createPost.html',
+        publicWall: 'publicWall.html',
+        publicUserProfile: 'publicUserProfile.html',
+        privateUserProfile: 'privateUserProfile.html'
+      }
+    }
+  }
+})
+
+
+
