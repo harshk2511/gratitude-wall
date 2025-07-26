b64f5471ab7dc660bf10592296ccdd23f738844b
Harshitha Kambham <harshk2511@gmail.com>
2025-07-26 21:42:24 +0530

    Made main screen responsive

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ const provider = new GoogleAuthProvider();
 // Selecting elements
 const googleSigninBtn = document.getElementById("google-signin-btn");
 const ssButtonsDiv = document.getElementById("small-screens-btn-div");
+const ssLoginBtn = document.getElementById("login-ss-btn");
+const ssSignUpBtn = document.getElementById("signup-ss-btn");
+const loginDiv = document.querySelector(".login-div");
+const signUpDiv = document.querySelector(".signup-div");
 
 // Google sign in
 googleSigninBtn.addEventListener("click", function() {
@@ -35,6 +39,40 @@ googleSigninBtn.addEventListener("click", function() {
 console.log(window.innerWidth);
 console.log(window.innerHeight);
 
-if (window.innerWidth >= 768) {
+window.addEventListener("DOMContentLoaded", () => {
+    if (window.innerWidth < 768) {
+    // On small screens: only show login, hide signup and show toggle buttons
+    loginDiv.style.display = "flex";
+    signUpDiv.style.display = "none";
+    ssButtonsDiv.style.display = "flex";
+
+    // Style the buttons correctly
+    ssLoginBtn.style.backgroundColor = "#E6B2BA";
+    ssLoginBtn.style.color = "white";
+    ssSignUpBtn.style.backgroundColor = "white";
+    ssSignUpBtn.style.color = "#E6B2BA";
+  } else {
+    // On large screens: show both blocks and hide toggle buttons
+    loginDiv.style.display = "flex";
+    signUpDiv.style.display = "flex";
     ssButtonsDiv.style.display = "none";
-}
+  }
+})
+
+ssLoginBtn.addEventListener("click", function() {
+    loginDiv.style.display = "flex";
+    signUpDiv.style.display = "none";
+    ssSignUpBtn.style.backgroundColor = "white";
+    ssSignUpBtn.style.color= "#E6B2BA";
+    ssLoginBtn.style.backgroundColor = "#E6B2BA";
+    ssLoginBtn.style.color= "white";
+})
+
+ssSignUpBtn.addEventListener("click", function() {
+    signUpDiv.style.display = "flex";
+    loginDiv.style.display = "none";
+    ssLoginBtn.style.backgroundColor = "white";
+    ssLoginBtn.style.color= "#E6B2BA";
+    ssSignUpBtn.style.backgroundColor = "#E6B2BA";
+    ssSignUpBtn.style.color= "white";
+})
